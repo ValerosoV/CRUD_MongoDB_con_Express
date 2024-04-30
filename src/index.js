d7baed739ecc3e6fcf8c77c091f0d0a056f98c69
Valero Vanegas - Juan David <juan.valero.9c@gmail.com>
2024-04-30 01:54:32 -0500

    Peticiones y ''main'' con comentarios
    -Se agregación las peticiones stack con protocolos online de transferencia de hypertextos (http) dentro de routes/sala_computo
    -El archivo index js de ya ha sido agregado las lineas que le dan funcionalidad para probar el programa
    -revisión de comentareados

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const parser = require("body-parser");            /*esta libreria vino con npm init*/     
 
-app.get('/EsoDictador',(req,res)=>{
-    res.send('Al alto mando aleman le gusta que desarrolle tecnologias')
-})
+const express = require('express');               /*la constante sera hecha usando la libreria back-end express*/
+const app = express();                            /*esta constante sera usada como una funcion llamada app*/
 
+const port = 3000;                               /*este es el puerto que usara la api*/
+const salasRoutes = require("./routes/sala_computo"); 
 
+const mongoose = require("mongoose");   /*Llama al gestor de rutas mongoose para ser usado*/
 
+require('dotenv').config()              /*solicita la informacion el archivo .env para configurar la conexión*/
+
+app.use(parser.urlencoded({ extended: false }));    //permite leer los datos que vienen en la petición
+app.use(parser.json());                             // transforma los datos a formato JSON
+
+//Gestión de las rutas usando el middleware
+app.use("/api", salasRoutes);                       //
+app.use(express.json());                            //express usara los datos convertidos a json que reciba
+
+//Conexión a la base de datos
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conexión exitosa"))    //cuando se logre correctamende muestre el mensaje por consola
+    .catch((error) => console.log(error));          //si salta error, atrapelo
+
+//Indicador del puerto usado para la conexión
 app.listen(port, () => {
-    console.log('La aplicación se esta ejecutando por el puerto'+ port)
-    }
-)
+    console.log(`Example app listening on port ${port}`)
+});
---
+++ src/routes/sala_computo.js
@@ -0,0 +1,68 @@
+const express = require("express");                 //llamado al framework
+const router = express.Router();                    //llamado al manejador de rutas de express
+
+const salas_computo = require("../model_schema/salas_computo"); //llamado al esquema en los modelos
+
+//Ingresar una Nueva sala de computo a la DB (R de Register en el CRUP)
+router.post("/salas_computo", (req, res) => {  /*peticion post que toma una ruta y una funcion flecha*/
+
+    const sala =  salas_computo(req.body); /*crea una constante con el esquema de la sala de computo como body de la peticion*/
+    sala      /*usando esta constante realice*/
+
+        .save()                                         //guardar el nuevo documento en la coleccion de la base de datos
+        .then((data) => res.json(data))                 //usando la variable data imprima la informacion de data en formato json
+        .catch((error) => res.json({ message: error }));//si sale error, atrapelo 
+});
+
+//Consulte todos los documentos de la colección  (C del Consult CRUD)
+router.get("/salas_computo", (req, res) =>  {   //petición de tipo get
+
+    salaSchema          /*con el esquema que disponemos*/
+
+        .find()                                      /* trigo todos los documentos en la colección que coincidan con la ruta */
+        .then((data) => res.json(data))                  // luego muetrelos
+        .catch((error) => res.json({ message: error })); //y que atrape un posible error
+});
+
+//Consulte un documento de la colección cuyo id sea el que le otorguemos (C del Consult CRUD)
+router.get("/salas_computo/:id", (req, res) => {
+
+    const { id } = req.params;
+    salaSchema
+
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Modificar el nombre de un animal por su id
+router.put("/salas_computo/:id", (req, res) => {
+
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    salaSchema
+
+        .updateOne({ _id: id }, {
+            $set: { nombre, edad, tipo, fecha }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//borrado del CRUD (D de Delete)
+router.delete("/salas_computo/:id", (req, res) => {
+
+    const { id } = req.params;
+    salaSchema
+
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+    }
+);
+
+module.exports = router; 
